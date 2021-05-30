1c88fe095f1f109ffc3a29afb133198f89c100f4
EversonFeltrin <efeltrin@inf.ufsm.br>
2021-05-29 22:13:07 -0300

    Improvements in routes to consider withdraw

--- src/routes.ts
+++ src/routes.ts
@@ -11,7 +11,7 @@ routes.get('/', (req, res) => {
 });
 
 // DEPOSIT ROUTES
-routes.post('/conta-corrente/depositar', (req: Request, res: Response) => {
+routes.post('/conta-corrente/depositar', (req: Request, res: Response): Promise<Response> => {
     return cashMachineController.cashDeposit(req.body, 'corrente')
         .then(response => {
             res.status(response.statusCode);
@@ -22,6 +22,26 @@ routes.post('/conta-corrente/depositar', (req: Request, res: Response) => {
 
 routes.post('/conta-poupanca/depositar', (req: Request, res: Response): Promise<Response> => {
     return cashMachineController.cashDeposit(req.body, 'poupanca')
+        .then(response => {
+            res.status(response.statusCode);
+
+            return res.json(response);
+        });
+});
+
+// WITHDRAW ROUTES
+routes.post('/conta-corrente/sacar', (req: Request, res: Response): Promise<Response> => {
+    return cashMachineController.cashWithdraw(req.body, 'corrente')
+        .then(response => {
+            res.status(response.statusCode);
+
+            return res.json(response);
+        });
+});
+
+routes.post('/conta-poupanca/sacar', (req: Request, res: Response): Promise<Response> => {
+    console.log('aqui vai sacar')
+    return cashMachineController.cashWithdraw(req.body, 'poupanca')
         .then(response => {
             res.status(response.statusCode);
 
