f215951d1e45bb1ac79ca5610cec9f09018f5589
EversonFeltrin <efeltrin@inf.ufsm.br>
2021-05-29 21:33:25 -0300

    Improvements in application to consider routes

--- src/application.ts
+++ src/application.ts
@@ -1,21 +1,20 @@
-import * as express from 'express'
-import * as bodyParser from 'body-parser'
-import * as cors from 'cors'
-import * as dotenv from 'dotenv'
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as cors from 'cors';
+import * as dotenv from 'dotenv';
+import { routes } from './routes';
 
-dotenv.config()
+dotenv.config();
 
-const application = express()
+const application = express();
 
-application.use(bodyParser.text())
-application.use(express.json())
-application.use(express.urlencoded({ extended: false }))
-application.use(cors())
+application.use(bodyParser.text());
+application.use(express.json());
+application.use(express.urlencoded({ extended: false }));
+application.use(cors());
 
-application.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+application.use(routes);
 
-application.set('port', process.env.APP_PORT || 5000)
+application.set('port', process.env.APP_PORT || 5000);
 
-export { application }
+export { application };
