4fd809e2ed836c8624da3cdb90cb14b3f22b8cbb
EversonFeltrin <efeltrin@inf.ufsm.br>
2021-05-29 22:12:12 -0300

    Improvements in controller to consider cash withdraw

--- src/controllers/CashMachineController.ts
+++ src/controllers/CashMachineController.ts
@@ -1,8 +1,24 @@
 import * as httpStatus from 'http-status';
 import {CashMachineModel} from '../models/CashMachineModel';
 
+/**
+ * @name Default Response
+ * @description Success response structure
+ * @param data Data response 
+ * @param statusCode Response status code
+ * @author Everson F. Feltrin
+ * @since 2021-05-29 
+ */
 const defaultResponse = (data: object, statusCode: number = httpStatus.OK) => ({ data, statusCode });
 
+/**
+ * @name Error Response
+ * @description Error response structure
+ * @param message Error message
+ * @param statusCode Response status code
+ * @author Everson F. Feltrin
+ * @since 2021-05-29
+ */
 const errorResponse = (message: string, statusCode: number = httpStatus.BAD_REQUEST) => defaultResponse({ error: message }, statusCode );
 
 interface ICashObject {
@@ -11,10 +27,20 @@ interface ICashObject {
 };
 
 class CashMachineController {
+    // DEPOSIT CONTROLLER
     async cashDeposit(data: ICashObject, type: string) {
         const cashMachineModel = new CashMachineModel();
 
         return cashMachineModel.deposit(data, type)
+            .then(response => defaultResponse(response))
+            .catch(error => errorResponse(error.message))
+    };    
+
+    // WITHDRAW CONTROLLER
+    async cashWithdraw(data: ICashObject, type: string) {
+        const cashMachineModel = new CashMachineModel();
+
+        return cashMachineModel.withdraw(data, type)
             .then(response => defaultResponse(response))
             .catch(error => errorResponse(error.message))
     };    
